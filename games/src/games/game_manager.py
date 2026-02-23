bf2347a2500401ac9a38f51dd9a761fd41bcb7b2
kshitij <kshitij.tomar07@berkeley.edu>
2026-02-23 11:10:09 -0800

    filepath changes to work with solver

--- games/src/games/game_manager.py
+++ games/src/games/game_manager.py
@@ -1,12 +1,14 @@
 from .clobber import Clobber
 from .horses import Horses
 from .pancakes import Pancakes
+from sokoban import Sokoban
 from models import *
 
 game_list = {
     "clobber": Clobber,
     "horses": Horses,
     "pancakes": Pancakes,
+    "sokoban": Sokoban
 }
 
 def validate(game_id: str, variant_id: str) -> bool:
